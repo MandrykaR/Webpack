3d8e79dd2e37a8712c89f98a05952cbc6b915c8f
MandrykaR <mandrykaroman01@gmail.com>
2024-01-28 16:04:21 +0100

    l2-t1

--- lesson1/task1/dist/scripts/calculator/calc.js
+++
@@ -1,3 +0,0 @@
-export const calc = (a, b) => a + b;
-
-export const mult = (a, b) => a * b;
---
+++ lesson2/task1/src/scripts/profile.js
@@ -0,0 +1,4 @@
+const printProfile = printProfile => {
+	const { name, company } = profileData
+	console.log(`${name} from ${company}`)
+}
