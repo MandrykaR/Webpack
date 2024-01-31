24af4ea36dd2cb7a86cef6f1fd283c96656590b3
MandrykaR <mandrykaroman01@gmail.com>
2024-01-31 21:39:30 +0100

    l4t1

--- lesson5/task1/webpack.config.js
+++ lesson5/task1/webpack.config.js
@@ -1,11 +1,10 @@
 const path = require('path')
 
 module.exports = {
-	entry: './src/main.js',
+	entry: './src/index.js',
 	output: {
 		filename: 'main.js',
 		path: path.resolve(__dirname, 'dist'),
 	},
-	mode: 'production',
-	devtool: 'eval-source-map',
+	mode: 'production', 
 }
